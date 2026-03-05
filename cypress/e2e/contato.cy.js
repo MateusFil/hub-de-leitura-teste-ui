ec6a4dac1146100459b1a980103a535608133067
Mateus Filipe <mateuswk@outlook.com>
2026-03-05 16:44:41 +0100

    Automação do Contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,76 @@
+describe('Funcionalidade: Contato', () => {
+
+  beforeEach(() => {
+    cy.visit('index.html')
+  });
+  
+
+
+  it('Deve preencher o formulário de contato com sucesso', () => {
+    
+    cy.get('[name="name"]').type('Mateus Filipe')
+    cy.get('[name="email"]').type('mateuswk@outlook.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('Mensagem de exemplo')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.contains('Contato enviado com sucesso!').should('exist')
+
+  })
+
+
+
+  it('Deve validar mensagem de erro ao enviar sem preencher o nome', () => {
+    cy.get('[name="name"]').clear
+    cy.get('[name="email"]').type('mateuswk@outlook.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('Mensagem de exemplo')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome')
+
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher o email', () => {
+
+
+    cy.get('[name="name"]').type('Mateus')
+    cy.get('[name="email"]').clear
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('Mensagem de exemplo')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail')
+
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher o assunto', () => {
+    cy.get('[name="name"]').type('Mateus')
+    cy.get('[name="email"]').type('mateuswk@outlook.com')
+    //cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('Mensagem de exemplo')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto')
+
+
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher a mensagem', () => {
+    cy.get('[name="name"]').type('Mateus')
+    cy.get('[name="email"]').type('mateuswk@outlook.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').clear
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem')
+
+
+  });
+
+  
+
+
+
+
+});
